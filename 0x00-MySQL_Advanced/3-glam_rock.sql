5708892983cd975b563b6a6da2fd5bebf8614642
KgomotsoNthuping <kgomotsonthuping22@gmail.com>
2024-06-13 14:31:41 +0000

    storage

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,3 @@
+-- Calculate lifespan
+SELECT band_name, (IFNULL(split, 2022) - formed)
+AS lifespan FROM metal_bands WHERE style LIKE '%Glam rock%' ORDER BY lifespan DESC;
---
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -0,0 +1,3 @@
+-- Create trigger to decrease
+CREATE TRIGGER decrease_items_quantity AFTER INSERT ON orders FOR EACH ROW
+UPDATE items SET quantity = quantity - NEW.number WHERE name=NEW.item_name;
